ea351fc59991b4bb7c58c086f477cbc5a4ae2e10
leylaneferreira <leylanegraziele@gmail.com>
2015-11-21 16:00:31 -0300

    implementando classe sanduiche

--- ISI/src/java/model/Sanduiche.java
+++ ISI/src/java/model/Sanduiche.java
@@ -1,9 +1,22 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Leylane
  */
 public class Sanduiche {
+
+    private String tipoPao;
+    private int tamanho;
+    private String recheio;
+    private String queijo;
+
+    private ArrayList<Boolean> adicionais;
+    private ArrayList<Boolean> vegetais;
+    private ArrayList<Boolean> molhos;
     
+    
+
 }
